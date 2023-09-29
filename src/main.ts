f2d2810d99440b175b283312b82996fdaaee68a1
Isaac <isaachkdeveloper@gmail.com>
2023-09-29 08:29:36 +0800

    !test

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,9 @@ async function bootstrap() {
   app.useGlobalInterceptors(new ResponseInterceptor());
   app.enableCors({
     origin:
-      process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : '*',
+      process.env.NODE_ENV === 'development'
+        ? 'http://localhost:3000'
+        : process.env.PORTAL_URL,
     methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
     credentials: true,
   });
